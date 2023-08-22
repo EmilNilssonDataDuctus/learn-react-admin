765b4fd2dd633d8385d28910e6531b611bd90a51
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-22 16:49:07 +0200

    Use guessed data from listGuesser to create new UserList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { Admin, ListGuesser, Resource } from "react-admin";
+import { Admin, Resource } from "react-admin";
+import { UserList } from "./Components/UserList/UserList";
 import { authProvider } from "./authProvider";
 import { dataProvider } from "./dataProvider";
 
 export const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
-    <Resource name="users" list={ListGuesser} />
+    <Resource name="users" list={UserList} />
   </Admin>
 );
