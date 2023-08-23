36956e996903171bc6fa959b9019046db5e55b18
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-23 17:23:12 +0200

    Add PostEdit component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { Admin, Resource } from "react-admin";
 import { PostsList } from "./Components/PostsList/PostsList";
 import { ResponsiveList } from "./Components/ResponsiveList/ResponsiveList";
 import { UserShow } from "./Components/UserDetails/UserDetails";
+import { PostEdit } from "./PostEdit/PostEdit";
 import { authProvider } from "./authProvider";
 import { dataProvider } from "./dataProvider";
 
 export const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
-    <Resource name="posts" list={PostsList} />
+    <Resource name="posts" list={PostsList} edit={PostEdit} />
     <Resource
       name="users"
       show={UserShow}
