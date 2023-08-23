4f033d123c793f7b9abfe38b4671015677fea5c4
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-23 16:41:30 +0200

    Create PostsList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Admin, Resource } from "react-admin";
+import { PostsList } from "./Components/PostsList/PostsList";
 import { ResponsiveList } from "./Components/ResponsiveList/ResponsiveList";
 import { authProvider } from "./authProvider";
 import { dataProvider } from "./dataProvider";
 
 export const App = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Resource name="posts" list={PostsList} />
     <Resource name="users" list={ResponsiveList} />
   </Admin>
 );
