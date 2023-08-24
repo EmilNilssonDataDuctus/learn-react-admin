922f872f24cd55a127786cefe32544aca8fedf28
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-24 17:24:05 +0200

    Move PostEdit component to component folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Admin, Resource } from "react-admin";
 import MyLoginPage from "./Components/Custom/MyLoginPage/MyLoginPage";
 import { PostCreate } from "./Components/PostCreate/PostCreate";
+import { PostEdit } from "./Components/PostEdit/PostEdit";
 import { PostShow } from "./Components/PostShow/PostShow";
 import { PostsList } from "./Components/PostsList/PostsList";
 import { ResponsiveList } from "./Components/ResponsiveList/ResponsiveList";
 import { UserShow } from "./Components/UserDetails/UserDetails";
-import { PostEdit } from "./PostEdit/PostEdit";
 import { authProvider } from "./authProvider";
 import { dataProvider } from "./dataProvider";
 
