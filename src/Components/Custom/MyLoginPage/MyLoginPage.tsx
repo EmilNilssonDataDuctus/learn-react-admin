cd07181f87a0b40a1f3c4542510cc2422d3ab427
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-24 16:59:15 +0200

    Fix login page

--- src/Components/Custom/MyLoginPage/MyLoginPage.tsx
+++ src/Components/Custom/MyLoginPage/MyLoginPage.tsx
@@ -1,32 +1,47 @@
+// in src/MyLoginPage.js
 import { useState } from "react";
 import { useLogin, useNotify } from "react-admin";
+import "./MyLoginPage.module.css";
 
-const MyLoginPage = ({}) => {
-  const [email, setEmail] = useState("");
+const MyLoginPage = () => {
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const login = useLogin();
   const notify = useNotify();
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    login({ email, password }).catch(() => notify("Invalid email or password"));
+    login({ username, password }).catch(() =>
+      notify("Invalid username or password")
+    );
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        name="email"
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        name="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-    </form>
+    <div
+      className="login-wrapper"
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "grid",
+        placeItems: "center",
+      }}
+    >
+      <form onSubmit={handleSubmit}>
+        <input
+          name="username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
+    </div>
   );
 };
 
