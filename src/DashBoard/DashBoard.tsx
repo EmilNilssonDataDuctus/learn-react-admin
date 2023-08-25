c0e882d88481f2ae2c0cb2d0d71f8576a9fc596e
Emil Nilsson <emil.nilsson@dataductus.se>
2023-08-25 10:02:39 +0200

    Install mui in my dependecies for easier auto import, create BasicSelect to use in dashboard

--- src/DashBoard/DashBoard.tsx
+++ src/DashBoard/DashBoard.tsx
@@ -1,8 +1,12 @@
 import { Card, CardContent, CardHeader } from "@mui/material";
+import BasicSelect from "../Components/Shared/BasicSelect";
 
 export const DashBoard = () => (
-  <Card>
-    <CardHeader title="Welcome to the internet" />
-    <CardContent>Have a look around</CardContent>
-  </Card>
+  <>
+    <Card>
+      <CardHeader title="Welcome to the internet" />
+      <CardContent>Have a look around</CardContent>
+    </Card>
+    <BasicSelect />
+  </>
 );
